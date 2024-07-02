acdec065a70a74bd85e29eecd8bcbd86f20b54ff
tinkeraudrey <audreyffm@gmail.com>
2024-07-01 21:17:40 -0400

    Initial Commit

---
+++ SAT App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAT App
+//
+//  Created by Audrey Lucas on 7/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
