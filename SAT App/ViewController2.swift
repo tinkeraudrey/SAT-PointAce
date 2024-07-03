fd07d1eea71abc918d97750c32f9af206f816582
tinkeraudrey <audreyffm@gmail.com>
2024-07-02 21:23:14 -0400

    Added a points system based on the total time elapsed.

--- SAT App/ViewController2.swift
+++ SAT App/ViewController2.swift
@@ -12,6 +12,14 @@ class ViewController2: UIViewController, TimerDelegate {
         return label
     }()
     
+    let pointsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textAlignment = .center
+        label.text = "Points: 0"
+        return label
+    }()
+    
     lazy var refreshButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Refresh", for: .normal)
@@ -25,27 +33,37 @@ class ViewController2: UIViewController, TimerDelegate {
         view.backgroundColor = .white
         
         view.addSubview(timeLabel)
+        view.addSubview(pointsLabel)
         view.addSubview(refreshButton)
         
         timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        pointsLabel.translatesAutoresizingMaskIntoConstraints = false
         refreshButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
             
+            pointsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pointsLabel.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 20),
+            
             refreshButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            refreshButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 20),
+            refreshButton.topAnchor.constraint(equalTo: pointsLabel.bottomAnchor, constant: 20),
         ])
         
         // Load totalElapsed from UserDefaults initially
         totalElapsed = UserDefaults.standard.double(forKey: "totalElapsed")
         
         updateTimeLabel() // Ensure time label is updated with stored value
+        updatePointsLabel() // Ensure points label is updated with stored value
     }
     
     func updateTimeLabel() {
         timeLabel.text = formattedString(for: totalElapsed)
+    }
+    
+    func updatePointsLabel() {
+        pointsLabel.text = "Points: \(Int(totalElapsed))"
     }
     
     func formattedString(for timeInterval: TimeInterval) -> String {
@@ -60,11 +78,14 @@ class ViewController2: UIViewController, TimerDelegate {
         // Reload totalElapsed from UserDefaults
         totalElapsed = UserDefaults.standard.double(forKey: "totalElapsed")
         updateTimeLabel()
+        updatePointsLabel()
     }
     
     // Implement TimerDelegate method to update totalElapsed
     func timerDidFinish(totalElapsed: TimeInterval) {
         self.totalElapsed = totalElapsed
         updateTimeLabel()
+        updatePointsLabel()
     }
 }
+
